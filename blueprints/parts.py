751edadda58a8016a591e67984f1cc3fe970e3fb
Stevan Pataki <stevanpataki@Stevans-MacBook-Pro.local>
2020-08-08 19:04:54 +0200

    dodato filtriranje delova po ceni i imenu

--- blueprints/parts.py
+++ blueprints/parts.py
@@ -13,6 +13,30 @@ def get_parts():
     parts = cursor.fetchall()
     return flask.jsonify(parts)
 
+@parts_blueprint.route("/partsPriceUp", methods=['GET'])
+def get_partsPriceUp():
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM pc_parts ORDER BY part_price DESC")
+
+    parts = cursor.fetchall()
+    return flask.jsonify(parts)
+
+@parts_blueprint.route("/partsPriceDown", methods=['GET'])
+def get_partsPriceDown():
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM pc_parts ORDER BY part_price ASC")
+
+    parts = cursor.fetchall()
+    return flask.jsonify(parts)
+
+@parts_blueprint.route("/partsPriceAbc", methods=['GET'])
+def get_partsPriceAbc():
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM pc_parts ORDER BY part_name")
+
+    parts = cursor.fetchall()
+    return flask.jsonify(parts)
+
 @parts_blueprint.route("/part/<int:part_id>", methods=['GET'])
 def get_part(part_id):
     cursor = mysql.get_db().cursor()
--- static/app/components/partsShow/partsShowCtrl.js
+++ static/app/components/partsShow/partsShowCtrl.js
@@ -3,52 +3,55 @@
     var app = angular.module("app");
 
 
-    app.controller("partsShowCtrl", ["$http" , "$state", function($http, $state) {
+    app.controller("partsShowCtrl", ["$http" , "$state","$scope", function($http, $state, $scope) {
         var that = this;
 
-        this.parts = []; 
-        this.getParts= function() {
+        $scope.parts = []; 
+        this.getParts = function() {
             $http.get("api/parts").then(function(result){
                 console.log(result);
-                that.parts = result.data;
+                $scope.parts = result.data;
+                console.log($scope.parts)
             },
             function(reason) {
                 console.log(reason);
             });
         }
 
-        // this.ukloniKorisnika = function(id) {
-        //     $http.delete("api/student/" + id).then(function(response){
-        //         console.log(response);
-        //         that.dobaviKorisnike();
-        //     },
-        //     function(reason){
-        //         console.log(reason);
-        //     });
-        // }
+        this.getPartsPriceUp = function() {
+            $http.get("api/partsPriceUp").then(function(result){
+                $scope.parts = [];
+                console.log(result);
+                $scope.parts = result.data;
+            },
+            function(reason) {
+                console.log(reason);
+            });
+        }
 
-        // this.dobaviPredmete= function() {
-        //     $http.get("api/predmet").then(function(result){
-        //         console.log(result);
-        //         that.predmet = result.data;
-        //     },
-        //     function(reason) {
-        //         console.log(reason);
-        //     });
-        // }
+        this.getPartsPriceDown = function() {
+            $http.get("api/partsPriceDown").then(function(result){
+                $scope.parts = [];
+                console.log(result);
+                $scope.parts = result.data;
+            },
+            function(reason) {
+                console.log(reason);
+            });
+        }
 
-        // this.ukloniPredmet = function(id) {
-        //     $http.delete("api/predmet/" + id).then(function(response){
-        //         console.log(response);
-        //         that.dobaviPredmete();
-        //     },
-        //     function(reason){
-        //         console.log(reason);
-        //     });
-        // }
+        this.getPartsPriceAbc = function() {
+            $http.get("api/partsPriceAbc").then(function(result){
+                $scope.parts = [];
+                console.log(result);
+                $scope.parts = result.data;
+            },
+            function(reason) {
+                console.log(reason);
+            });
+        }
 
         this.getParts();
-        // this.dobaviPredmete();
 
     }]);
 })(angular);
