dd2ce26be2b602a1ee3810955d5d1ea6eef48e56
Stevan Pataki <stevanpataki@Stevans-MacBook-Pro.local>
2020-08-20 03:09:28 +0200

    dodoata order skroz radi

--- blueprints/orders.py
+++ blueprints/orders.py
@@ -38,12 +38,24 @@ def create_order():
     response["adress_street"] = temp[0]["adress_id"]
 
     print(response)
-    #! OVAJ QUERY NE RADI
     cursor.execute("INSERT INTO orders (order_price, order_date, order_status, user_user_id, adress_adress_id, adress_city_city_id) VALUES(%(order_price)s, %(order_date)s, %(order_status)s, %(user_id)s, %(adress_street)s, %(city_name)s)", response)
     db.commit()
 
-    cursor.execute("SELECT * FROM order")
-    test = cursor.fetchall()
-    print(test)
+    cursor.execute("SELECT LAST_INSERT_ID()")
+    last_order_id =  cursor.fetchone()
+    response["order_status"] = last_order_id['LAST_INSERT_ID()']
+
+    cursor.execute("SELECT part_stock FROM pc_parts WHERE part_id=%(part_id)s ", response)
+    temp =  cursor.fetchall()
+    temp = temp[0]['part_stock']
+    response['pices'] = temp - response['pices']
+
+    cursor.execute("UPDATE pc_parts SET part_stock = %(pices)s WHERE part_id=%(part_id)s;", response)
+    db.commit()
+
+    cursor.execute("INSERT INTO orders_has_pc_parts (orders_order_id, orders_user_user_id, orders_adress_adress_id, orders_adress_city_city_id, pc_parts_part_id) VALUES(%(order_status)s, %(user_id)s, %(adress_street)s, %(city_name)s, %(part_id)s)", response)
+    db.commit()
+
+    print("IZVRSENO")
 
     return flask.jsonify(flask.request.json), 201
